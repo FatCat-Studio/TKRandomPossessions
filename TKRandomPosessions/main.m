f4ffe539d02eda46139afd6fc188bc3b81bfc3a9
Timofey Korchagin <tkorchagin@gmail.com>
2012-04-21 17:58:43 +0400

    Initial Commit

---
+++ TKRandomPosessions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TKRandomPosessions
+//
+//  Created by Timofey Korchagin on 21/04/2012.
+//  Copyright (c) 2012 MIPT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
